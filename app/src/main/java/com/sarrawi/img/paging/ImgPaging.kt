23b040e54b1c406480c46d05e8cb21dba8fee3f2
MohammedOss <abdallahomar1992@gmail.com>
2023-11-13 19:49:23 +0300

    Full

--- app/src/main/java/com/sarrawi/img/paging/ImgPaging.kt
+++ app/src/main/java/com/sarrawi/img/paging/ImgPaging.kt
@@ -1,79 +1,114 @@
 package com.sarrawi.img.paging
 
 import android.util.Log
+import androidx.paging.LoadType
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.sarrawi.img.Api.ApiService
 import com.sarrawi.img.model.ImgsModel
 import com.sarrawi.img.model.ImgsRespone
 
-class ImgPaging(private val apiService: ApiService,var ID_Type_id: Int) : PagingSource<Int, ImgsModel>() {
+class ImgPaging(private val apiService: ApiService, var ID_Type_id: Int) : PagingSource<Int, ImgsModel>() {
 
-    private val TAG:String = "TAG_PAGING"
+    private val TAG: String = "TAG_PAGING"
+
+    private var lastLoadedPage: Int? = null
+
     override fun getRefreshKey(state: PagingState<Int, ImgsModel>): Int? {
-        return state.anchorPosition?.let {
-            val closestPage = state.closestPageToPosition(it)
-            closestPage?.prevKey?.takeIf { it != null && it != Int.MIN_VALUE }
-        }
+        return null
     }
-//    override fun getRefreshKey(state: PagingState<Int, ImgsModel>): Int? {
-//        val refreshKey = state.anchorPosition?.let {
-//            state.closestPageToPosition(it)?.prevKey?.plus(1) ?: state.closestPageToPosition(it)?.nextKey?.minus(1)
-//        }
-//        Log.d(TAG, "getRefreshKey: $refreshKey")
-//        return refreshKey
-//    }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ImgsModel> {
+    override suspend fun load(params: LoadParams<Int>):
+            LoadResult<Int, ImgsModel> {
         try {
-            val page = params.key ?: 1
-            // استخدام ID_Type_id كمعامل
-            val response = apiService.getImgsData(ID_Type_id, page)
-            if (response.isSuccessful) {
-                val data = response.body()?.results ?: emptyList()
-                val prevKey = if (page == 0) null else page - 1
-                //val nextKey = if (data.isEmpty()) null else page + 1
-                Log.d(TAG, "load: currentPage is ${response.body()?.currentPage} and total is ${response.body()?.totalPages}")
-                val nextKey = if(response.body()?.currentPage == response.body()?.totalPages) null else page+1
-                Log.d(TAG, "load: nextKey is $nextKey and prevKeyis $prevKey anf page = $page id = $ID_Type_id")
-                // إعادة البيانات ومفاتيح الصفحات
-                Log.d(TAG,"${response.body()?.results?.size}")
-                return LoadResult.Page(data = data, prevKey = prevKey, nextKey = nextKey)
-            } else {
-                // إذا كان هناك خطأ في الاستدعاء
-                return LoadResult.Error(Exception("Error loading data"))
+            val currentPage = params.key ?: 1
+
+            // تحقق مما إذا كانت هذه الصفحة هي نفس الصفحة التي تم تحميلها آخر مرة
+            if (currentPage == lastLoadedPage) {
+                // قم بإرجاع نتيجة فارغة للإشارة إلى عدم وجود بيانات جديدة
+                return LoadResult.Page(emptyList(), prevKey = null, nextKey = null)
             }
+
+            val response = apiService.getImgsData(ID_Type_id, currentPage)
+            val responseData = mutableListOf<ImgsModel>()
+            val data = response.body()?.results ?: emptyList()
+            responseData.addAll(data)
+
+            // قم بتحديث الصفحة التي تم تحميلها آخر مرة
+//            lastLoadedPage = currentPage
+
+            // قم بحساب المفتاح التالي بناءً على ما إذا كانت هناك بيانات في الاستجابة
+            val nextKey = if (data.isNotEmpty()) currentPage + 1 else null
+
+            return LoadResult.Page(
+                data = responseData,
+                prevKey = if (currentPage == 1) null else currentPage - 1,
+                nextKey = nextKey
+            )
         } catch (e: Exception) {
-            // إذا حدث استثناء أثناء الاستدعاء
             return LoadResult.Error(e)
         }
     }
+
+    fun updateID_TypeId(newID: Int) {
+        ID_Type_id = newID
+    }
+}
+
+//    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ImgsModel> {
+//        try {
+//            val currentPage = params.key ?: 1
 //
-//    override fun getRefreshKey(state: PagingState<Int, ImgsModel>): Int? {
-//        return state.anchorPosition?.let {
-//            state.closestPageToPosition(it)?.prevKey?.plus(1)?:state.closestPageToPosition(it)?.nextKey?.minus(1)
+//            // Check if this page is the same as the last loaded page
+//            if (currentPage == lastLoadedPage) {
+//                // Return an empty result to indicate no new data
+//                return LoadResult.Page(emptyList(), prevKey = null, nextKey = null)
+//            }
+//
+//            val response = apiService.getImgsData(ID_Type_id, currentPage)
+//            val responseData = mutableListOf<ImgsModel>()
+//            val data = response.body()?.results ?: emptyList()
+//            responseData.addAll(data)
+//
+//            // Update the last loaded page
+//            lastLoadedPage = currentPage
+//
+//            return LoadResult.Page(
+//                data = responseData,
+//                prevKey = if (currentPage == 1) null else currentPage - 1,
+//                nextKey = currentPage + 1
+//            )
+//        } catch (e: Exception) {
+//            return LoadResult.Error(e)
 //        }
+//    }
+
+
+
+
+//    override fun getRefreshKey(state: PagingState<Int, ImgsModel>): Int? {
+//        val anchorPosition = state.anchorPosition ?: return null
+//        val closestPage = state.closestPageToPosition(anchorPosition)
+//
+//        // Check if the user is scrolling to the top
+//        if (closestPage?.prevKey != null && state.pages.size > 1 && state.pages[1].prevKey != null) {
+//            return null
+//        }
+//
+//        // If scrolling down or initial load, use current page key
+//        return state.pages.firstOrNull()?.prevKey
 //    }
 //
 //    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ImgsModel> {
 //        try {
 //            val page = params.key ?: 1
-//
 //            // استخدام ID_Type_id كمعامل
 //            val response = apiService.getImgsData(ID_Type_id, page)
-//
 //            if (response.isSuccessful) {
 //                val data = response.body()?.results ?: emptyList()
-//                val prevKey = if (page == 0) null else page - 1
+//                val prevKey = if (page == 0) null  else page + 1
+//                val nextKey = if (response.body()?.currentPage == response.body()?.totalPages) null else page + 1
 //
-//
-//                //val nextKey = if (data.isEmpty()) null else page + 1
-//                Log.d(TAG, "load: currentPage is ${response.body()?.currentPage} and total is ${response.body()?.totalPages}")
-//                val nextKey = if(response.body()?.currentPage == response.body()?.totalPages) null else page+1
-//
-//                Log.d(TAG, "load: nextKey is $nextKey and prevKeyis $prevKey anf page = $page id = $ID_Type_id")
-//                // إعادة البيانات ومفاتيح الصفحات
-//                Log.d(TAG,"${response.body()?.results?.size}")
 //                return LoadResult.Page(data = data, prevKey = prevKey, nextKey = nextKey)
 //            } else {
 //                // إذا كان هناك خطأ في الاستدعاء
@@ -85,7 +120,28 @@ class ImgPaging(private val apiService: ApiService,var ID_Type_id: Int) : Paging
 //        }
 //    }
 
-    fun updateID_TypeId(newID: Int) {
-        ID_Type_id = newID
-    }
-}
+
+
+//    override suspend fun load(params: LoadParams<Int>):
+//            LoadResult<Int, ImgsModel> {
+//
+//        return try {
+//            val currentPage = params.key ?: 1
+//            val response = apiService.getImgsData(ID_Type_id, currentPage)
+//            val responseData = mutableListOf<ImgsModel>()
+//            val data = response.body()?.results ?: emptyList()
+//            responseData.addAll(data)
+//
+//            LoadResult.Page(
+//                data = responseData,
+//                prevKey = if (currentPage == 1) null else -1,
+//                nextKey = currentPage.plus(1)
+//            )
+//        } catch (e: Exception) {
+//            LoadResult.Error(e)
+//        }
+//
+//    }
+
+
+
